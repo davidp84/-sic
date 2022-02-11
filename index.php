2fc8d429429ed156bc62015081c5b0c203e532ba
davidp84 <62162575+davidp84@users.noreply.github.com>
2022-02-11 19:21:06 +1100

    add comments, remove redundant code - index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $result = '';
 topModule("Homelink");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  require("post-validation-seller.php");
+  require("post-validation-seller.php"); // Required for server side validation
 
   if (isset($_POST['status'])) {
     LogIO();
@@ -18,7 +18,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     unset($_SESSION['user']);
   }
 
-  // Need to adjust $_POST to suit correct variable
+  // sets the SESSION data dependant on which form has been filled
+  // and calls the method to create the relevant block. Also unsets 
+  // the POST data. 
   if ($_POST['variant'] == "permit") {
   $_SESSION['propertyAddress'] = $_POST['address'];
   $_SESSION['ownerDetails'] = $_POST['name'];
@@ -52,14 +54,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $result = createBankBlock();
  }
 
-//  if (!isset($_SESSION['propertyAddress']) || $_SESSION['propertyAddress'] == null) {
-//   errorMessage();
-// } else {
-  //run else if's for the different forms. Via $_POST['variant'].
-  // $result = createSellerBlock();
-    // unset($_SESSION);
-// }
-
 }  
   navContent();   
     
@@ -70,34 +64,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <h1>Home Page</h1>
       <h2>Create Permit Application</h2>
       <form class='shop-form' method='post' action=""  >
+      <input type='hidden' id="variant" name='variant' value="permit" />
       <p>Property Address</p>
-      <input class="address" type='text' id="address" name='address' pattern="^[\da-zA-Z '\-\/.,]+$" value=""/><br><br>
+      <input class="address" type='text' id="address" name='address' pattern="^[\da-zA-Z '\-\/.,]+$" value=""/>
       <p>Owner/Vendor Details</p>
       <input class="textfield" type='text' id="name" name='name' pattern="^[a-zA-Z '\-.]+$" value=""/>
       <p>Building Design</p>
       <input class="textfield" type='text' id="design" name='design' value=""/>
       <p>Seller Licence Number</p>
       <input class="textfield" type='text' id="licence" name='licence' value=""/>
-      <input class="order-button" type='submit' id="variant" name='permit' value='Create Permit Application' >
+      <input class="order-button" type='submit' name='permit' value='Create Permit Application' >
       </form>
 
       <h2>Authority Approval</h2>
       <form class='shop-form' method='post' action=""  >
+      <input type='hidden' id="variant" name='variant' value="authority" />
       <p>Decision</p>
       <input class="textfield" type='text' id="decision" name='decision' value=""/>
       <p>Property Address</p>
       <input class="address" type='text' id="address" name='address' value=""/><br><br>
-      <input class="order-button" type='submit' id="variant" name='authority' value='Create Athority Approval'>
+      <input class="order-button" type='submit' name='authority' value='Create Athority Approval'>
       </form>
 
       <h2>Loan Application</h2>
       <form class='shop-form' method='post' action=""  >
+      <input type='hidden' id="variant" name='variant' value="loan" />
       <p>Full Name</p>
       <input class="textfield" type='text' id="name" name='name' pattern="^[a-zA-Z '\-.]+$" value=""/>
       <p>Date Of Birth</p>
       <input class="textfield" type='text' id="dob" name='dob' value=""/>
       <p>Current Address</p>
-      <input class="address" type='text' id="currentAddress" name='currentAddress' value=""/><br><br>
+      <input class="address" type='text' id="currentAddress" name='currentAddress' value=""/>
       <p>Phone Number</p>
       <input class="textfield" type='text' id="number" name='number' pattern="^^(\(0\d\)|0\d|\+61\d|)( ?\d){8}$" value=""/>
       <p>Employer Name</p>
@@ -105,25 +102,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <p>Annual Income</p>
       <input class="textfield" type='text' id="income" name='income' value=""/>
       <p>Address of the Property intended to buy</p>
-      <input class="address" type='text' id="address" name='address' value=""/><br><br>
+      <input class="address" type='text' id="address" name='address' value=""/>
       <p>Loan Amount</p>
       <input class="textfield" type='text' id="loan" name='loan' value=""/>
-      <input class="order-button" type='submit' id="variant" name='loan' value='Create Loan Application'>
+      <input class="order-button" type='submit' name='loan' value='Create Loan Application'>
       </form>
 
       <h2>Bank Loan Approval</h2>
       <form class='shop-form' method='post' action=""  >
+      <input type='hidden' id="variant" name='variant' value="bank" />
       <p>Decision</p>
       <input class="textfield" type='text' id="decision" name='decision' value=""/>
       <p>Full Name</p>
       <input class="textfield" type='text' id="name" name='name' pattern="^[a-zA-Z '\-.]+$" value=""/>
       <p>Current Address</p>
-      <input class="address" type='text' id="address" name='address' value=""/><br><br>
+      <input class="address" type='text' id="address" name='address' value=""/>
       <p>Phone Number</p>
       <input class="textfield" type='text' id="number" name='number' pattern="^^(\(0\d\)|0\d|\+61\d|)( ?\d){8}$" value=""/>
       <p>Date Of Birth</p>
       <input class="textfield" type='text' id="dob" name='dob' value=""/>
-      <input class="order-button" type='submit' id="variant" name='bank' value='Create Bank Loan Approval'>
+      <input class="order-button" type='submit' name='bank' value='Create Bank Loan Approval'>
     </form>
 </main>
 
